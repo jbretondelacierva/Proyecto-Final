637c7242774e39f723276f39b456ba2dd6ecd945
beatrizsicilia <beatrizsicilia@icloud.com>
2022-05-22 11:51:10 +0200

    c

---
+++ src/test/java/com/grupopat/practicafinal/controller/GastosE2ETest.java
@@ -0,0 +1,58 @@
+package com.grupopat.practicafinal.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
+
+import java.util.List;
+
+import com.grupopat.practicafinal.model.EventosModel;
+import com.grupopat.practicafinal.model.GastoModel;
+import com.grupopat.practicafinal.model.UserModel;
+import com.grupopat.practicafinal.repository.GastoRepository;
+import com.grupopat.practicafinal.repository.UserRepository;
+import com.grupopat.practicafinal.service.EventosService;
+import com.grupopat.practicafinal.service.GastoService;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class GastosE2ETest {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private GastoRepository gastoRepository;
+
+    @Autowired
+    private GastoService gastoService; 
+
+    @Test
+    public void getEventosByUser(){
+        
+        Iterable<GastoModel> response = gastoService.getGastosByPeriodo("Verano","user1");
+        String url = "http://localhost:" + Integer.toString(port) + "/api/v1/gastos/periodo/Verano/user1";
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Basic dXNlcjE6MTIz");
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<Iterable<GastoModel>> result = restTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                entity,
+                new ParameterizedTypeReference<Iterable<GastoModel>>(){}
+        );
+
+        then(result.getStatusCode()).isEqualTo(HttpStatus.OK);
+        then(result.getBody()).isEqualTo(response);
+
+    }
+}
+
